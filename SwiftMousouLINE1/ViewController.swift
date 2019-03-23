28a8975f13ca26e10523124bbd72669b8afbcd34
modeverv <modeverv@gmail.com>
2019-03-23 21:18:23 +0900

    Initial Commit

---
+++ SwiftMousouLINE1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMousouLINE1
+//
+//  Created by seijiro on 2019/03/23.
+//  Copyright © 2019 norainu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
